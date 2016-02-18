ae3a152eba8332da55173cc2260e89511f308188
Joni Halabi <joni@jhalabi.com>
2016-02-18 00:44:46 -0500

    Feedback on JS milestone

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    /*
+     This is fabulous!!  :)
+    */
+
     $('#tabs li').hover(
     function () {
         //show submenu
@@ -7,6 +11,15 @@ $(document).ready(function () {
         //hide submenu
         $('ul', this).hide();
     });
+
+/*
+    This click event is crossing everything out any time the user
+    clicks anywhere inside the form (which has that ID of #instructions). 
+    Is that right?
+
+    If you just want people to cross out individual items, you can apply
+    the click event to the checkbox input fields instead.
+*/
 
 $('#instructions').on('click', crossout);
 
@@ -23,6 +36,26 @@ function crossout() {
 
 $("input:checkbox").click(function () {
             var listInstruction = $(this);
+            /*
+                I don't think this line is doing what you intend it to do.
+                It is literally adding classes to the input field that was clicked,
+                called 'done:checked + span'.  So, in HTML, the checkbox looks like this:
+
+                <input type="checkbox" name="step" value="preheat" class="done:checked + span">
+
+                The .toggleClass() function should only be passed the name of the class you want
+                to add/remove.  If you also want to display the input checkbox as being checked,
+                you could do something like this:
+
+                if (listInstruction.attr('checked')) {
+                    listInstruction.removeAttr('checked');
+                } else {
+                    listInstruction.attr('checked', 'checked');
+                }
+                
+                Sadly, there is no "toggleAttribute" function in jQuery, so you do need
+                to use an if statement for this.
+            */
             listInstruction.toggleClass('done:checked + span');
  
         });
